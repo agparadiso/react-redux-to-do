f67f92b5dd8bd102183eb2bcbb8777910a31960c
Gabriel <gaboparadiso@gmail.com>
2016-10-02 22:33:36 +0200

    unshift instead of push

--- reducers/todos.js
+++ reducers/todos.js
@@ -3,7 +3,7 @@ import Inmutable from 'immutable'
 export default (state = Inmutable.List(['Code More!']), action) => {
   switch (action.type) {
     case 'addTodo':
-      return state.push(action.todo)
+      return state.unshift(action.todo)
     default:
       return state
   }
